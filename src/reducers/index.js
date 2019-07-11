7c896bb04acdde7ceeef97a300988662cce0d3a1
Mark <jardenilmark@gmail.com>
2019-07-11 22:50:29 +0800

    feat: basic login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
 import user from './user'
 
 export default combineReducers({
-  form: formReducer,
   user: user
 })
