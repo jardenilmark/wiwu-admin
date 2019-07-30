96b8de4d8f31c3db481dff15297d40af3c47822c
Mark <jardenilmark@gmail.com>
2019-07-30 17:44:18 +0800

    feat: logout button

---
+++ src/actions/user/logout.action.js
@@ -0,0 +1,16 @@
+import { auth } from '../../firebase'
+import { LOGIN_SUCCESS, LOGIN_FAILED } from './user.constants'
+import { createAction } from 'redux-actions'
+
+export const logout = () => {
+  return async dispatch => {
+    try {
+      await auth.signOut()
+      // alert(auth.currentUser.emailVerified)
+      dispatch(createAction(LOGIN_SUCCESS))
+    } catch (error) {
+      console.log('error', error)
+      dispatch(createAction(LOGIN_FAILED)(error.message))
+    }
+  }
+}
