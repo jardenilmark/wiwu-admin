2625ed7a7b038245daf163b382f14f7b2a126547
Danddie Porras <danddieporras8@gmail.com>
2019-09-05 22:52:30 +0800

    fix: fix setAuthDetails action

--- src/actions/admin/setAuthDetails.action.js
+++ src/actions/admin/setAuthDetails.action.js
@@ -6,15 +6,19 @@ import { SET_AUTH_DETAILS } from './admin.constants'
 export const setAuthDetails = (user, loading, authenticated) => {
   return async dispatch => {
     try {
-      const { uid, email, emailVerified } = user
+      let userPayload
 
-      const ref = await firestore
-        .collection('users')
-        .doc(uid)
-        .get()
-      const userData = ref.data()
+      if (user) {
+        const { uid, email, emailVerified } = user
 
-      const userPayload = { uid, email, emailVerified, ...userData }
+        const ref = await firestore
+          .collection('users')
+          .doc(uid)
+          .get()
+        const userData = ref.data()
+
+        userPayload = { uid, email, emailVerified, ...userData }
+      }
 
       dispatch(
         createAction(SET_AUTH_DETAILS)({ userPayload, loading, authenticated })
