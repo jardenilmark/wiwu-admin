3cc30706dfa46e084b5283085a11a0435437013b
Danddie Porras <danddieporras8@gmail.com>
2019-09-17 14:52:32 +0800

    fix(AuthRoute): change the path to redirect to depending on the user role

--- src/components/routes/AuthRoute.js
+++ src/components/routes/AuthRoute.js
@@ -7,14 +7,14 @@ import UnverifiedScreen from '../auth/UnverifiedScreen'
 
 const AuthRoute = ({ component: Component, ...rest }) => {
   const user = useSelector(state => state.admin.current)
-
+  const path = user.role === 'admin' ? 'manage-responders' : 'manage-contacts'
   return (
     <Route
       {...rest}
       render={props => {
         if (user && (user.role === 'admin' || user.role === 'responder')) {
           if (user.emailVerified) {
-            return <Redirect to='/admin-page/manage-responders' />
+            return <Redirect to={`/admin-page/${path}`} />
           } else {
             return <UnverifiedScreen />
           }
