f0a9fb4fdf6eff728504b2afbc7146e9f38f62d5
Danddie Porras <danddieporras8@gmail.com>
2019-09-30 22:19:31 +0800

    fix(Sidebar): render when activeKey is not null

--- src/helpers/common/getActiveKey.js
+++ src/helpers/common/getActiveKey.js
@@ -1,7 +1,8 @@
 export const getActiveKey = location => {
+  console.log('Location - ', location)
   const { pathname } = location
   const paths = pathname.split('/')
   const activeKey = paths[2]
-
+  console.log('active - ', activeKey)
   return activeKey
 }
